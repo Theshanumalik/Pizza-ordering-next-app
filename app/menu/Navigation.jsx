920574533dec496620c977b68da01dce040118bb
theshanumalik <shanumalik9001567.com>
2023-11-10 14:31:25 +0530

    initial commit

--- app/menu/Navigation.jsx
+++ app/menu/Navigation.jsx
@@ -1,16 +1,44 @@
-import React from "react";
+"use client";
+
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Navigation() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const [filter, setFilter] = useState(() => searchParams.get("filter") || "");
+  const [sortBy, setSortBy] = useState(() => searchParams.get("sort") || "");
+  const { replace } = useRouter();
+  useEffect(() => {
+    const params = new URLSearchParams(searchParams);
+    if (sortBy !== "") {
+      params.set("sort", sortBy);
+    } else {
+      params.delete("sort");
+    }
+    if (filter !== "") {
+      params.set("filter", filter);
+    } else {
+      params.delete("filter");
+    }
+    replace(`${pathname}?${params}`);
+  }, [filter, sortBy]);
   return (
     <div className="flex justify-between py-4 mb-4 max-sm:flex-col gap-4">
       <h1 className="text-xl font-medium">Menu</h1>
       <div className="flex gap-3">
-        <select className="bg-transparent border-red-500 border p-3 rounded-md cursor-pointer">
+        <select
+          className="bg-transparent border-red-500 border p-3 rounded-md cursor-pointer"
+          onChange={(e) => setSortBy(e.target.value)}
+        >
           <option value="">Sort By</option>
           <option value="name">Name</option>
           <option value="price">Price</option>
         </select>
-        <select className="bg-transparent border-red-500 border p-3 rounded-md cursor-pointer">
+        <select
+          className="bg-transparent border-red-500 border p-3 rounded-md cursor-pointer"
+          onChange={(e) => setFilter(e.target.value)}
+        >
           <option value="">Filter By</option>
           <option value="veg">Veg</option>
           <option value="non-veg">Non veg</option>
--- app/menu/page.jsx
+++ app/menu/page.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Navigation from "./Navigation";
 import Cards from "@/components/Cards";
-import { products } from "@/tempDB/products";
+import dbConnect from "@/config/dbConnect";
+import Pizza from "../../model/Pizza";
 
-export default function page() {
+export default async function page({ searchParams }) {
+  const products = await getProducts(searchParams);
   return (
     <section className="p-3">
       <div className="max-w-container mx-auto">
@@ -12,4 +14,19 @@ export default function page() {
       </div>
     </section>
   );
+}
+
+async function getProducts(searchParams) {
+  const searchQuery = {};
+  if (searchParams?.filter) {
+    searchQuery.category = searchParams?.filter;
+  }
+  try {
+    console.log({ [searchParams?.sort]: -1 });
+    await dbConnect();
+    return await Pizza.find(searchQuery).sort({ [searchParams?.sort]: -1 });
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 }
